751f8994518bfe8c2b4fbed31fb0a648b32c6b9e
Danielle <danineubauerr@gmail.com>
2019-12-14 10:05:04 -0800

    edits

--- liri.js
+++ liri.js
@@ -1,5 +1,40 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
+// var spotify = new Spotify(keys.spotify);
 
-var spotify = new Spotify(keys.spotify);
+var 
+
+var theme = process.argv[2]; 
+var search = process.argv.slice(3).join(' '); 
+
+//If no search happened: 
+if (!theme) { 
+    search = 'concert-this';
+}
+if (!search) { 
+    search = 'Beyonce';
+}
+
+if (theme === 'concert-this') { 
+    console.log('searching for a concert');
+
+} else if (theme === 'spotify-this-song') { 
+    console.log('searching for a song');
+    
+} else if (theme === 'movie-this') { 
+    console.log('searching for a movie'); 
+} else if (theme === 'do-what-it-says') { 
+    console.log('do what it says')
+}
+
+
+//node liri.js concert-this <artist/band name here>
+// concert-this
+    //name, venue location, date of event (moment)
+    //codingbootcamp as app_id
+        //https://rest.bandsintown.com/artists/celine+dion/events?app_id=codingbootcamp
+// spotify-this-song
+    //artist, name, Spotify link, album 
+// movie-this
+// do-what-it-says
